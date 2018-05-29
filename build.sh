e73ccde7e0e2060b671e8f2c7b14a2aedb5dce12
Andreas <andreas.haeferer@keatec.com>
2018-05-29 13:12:56 +0200

    added Link To Rabbit Container

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ docker stop sparkserver
 
 docker rm sparkserver
 
-docker run -e "LOG_LEVEL=debug" -p 8100:8080 -p 5683:5683 --volume /localStore/data:/usr/src/localCloud/spark-rabbit/data -d --name sparkserver --hostname sparkserver keatec/spark-rabbit:nightly
+docker run -e "LOG_LEVEL=debug" -p 8100:8080 -p 5683:5683 --volume /localStore/data:/usr/src/localCloud/spark-rabbit/data --link rabbit:rabbit -d --name sparkserver --hostname sparkserver keatec/spark-rabbit:nightly
 
 #docker rm $(docker ps -q -f status=exited) & docker rmi $(docker images -f "dangling=true" -q)
 
